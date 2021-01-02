676bdab3be9d11996e5db69478455f5364526c93
shrued <shrutisara10@gmail.com>
2021-01-02 20:05:50 +0530

    routes

--- app.py
+++ app.py
@@ -2,8 +2,12 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/")
-def joey():
+def jeoy():
   return 'How you doin'
 
+@app.route("/<name>")
+def name(name):
+  return 'Hey ' + name
+
 if __name__ == "__main__":
-  app.run()
+  app.run(debug=True)
